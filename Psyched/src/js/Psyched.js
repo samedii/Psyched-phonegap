f460b9664ecb7e031d31a5f6201c156115014a53
samedii <samedii@gmail.com>
2015-01-18 03:28:29 +0100

    ngRoute again

--- Psyched/src/js/Psyched.js
+++ Psyched/src/js/Psyched.js
@@ -4,55 +4,34 @@
     angular.module('Psyched', [
         'ngRoute',
         'mobile-angular-ui',
+        'Navigation',
         'Graph',
         'Journal',
         'Profile',
         'Form'
     ])
 
-    .config([
-        '$stateProvider',
-        '$urlRouterProvider',
-        function($routeProvider) {
+    .config(function($routeProvider) {
 
-            $routeProvider
-                .when('graph', {
-                    url: '/graph/:graph',
-                    templateUrl: 'partials/graph.html',
-                    controller: 'GraphCtrl'
-                })
-                .when('profile', {
-                    url: '/profile',
-                    templateUrl: 'partials/profile.html',
-                    controller: 'ProfileCtrl'
-                })
-                .when('journal', {
-                    url: '/journal',
-                    templateUrl: 'partials/journal.html',
-                    controller: 'JournalCtrl'
-                })
-                .when('form', {
-                    url: '/form/:form',
-                    templateUrl: 'partials/form.html',
-                    controller: 'FormCtrl'
-                });
+        $routeProvider
+            .when('/graph/:graph', {
+                templateUrl: 'partials/graph.html',
+                controller: 'GraphCtrl'
+            })
+            .when('/profile', {
+                templateUrl: 'partials/profile.html',
+                controller: 'ProfileCtrl'
+            })
+            .when('/journal', {
+                templateUrl: 'partials/journal.html',
+                controller: 'JournalCtrl'
+            })
+            .when('/form/:form', {
+                templateUrl: 'partials/form.html',
+                controller: 'FormCtrl'
+            })
+            .otherwise('/profile');
 
-            $urlRouterProvider.when('/graph', '/graph/');
-
-            $urlRouterProvider.otherwise('/profile');
-
-            $urlRouterProvider.rule(function($injector, $location) {
-                //what this function returns will be set as the $location.url
-                var path = $location.path(),
-                    normalized = path.toLowerCase();
-                if (path != normalized) {
-                    //instead of returning a new url string, I'll just change the $location.path directly so I don't have to worry about constructing a new url string and so a new state change is not triggered
-                    $location.replace().path(normalized);
-                }
-                // because we've returned nothing, no state change occurs
-            });
-
-        }
-    ]);
+    });
 
 })();
