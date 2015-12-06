62b9525ae5181b38c559d2f8ef4fc15994c11d38
samedii <samedii@gmail.com>
2015-12-06 14:32:29 +0100

    Test changes

--- Psyched/js/controllers/Tests.js
+++ Psyched/js/controllers/Tests.js
@@ -28,29 +28,36 @@
     tests.run(function(addTest) {
         addTest('apeIndex', {type: 'measurement', english: 'Ape index', unit: 'cm'});
         addTest('campusBoard', {type: 'challenge', english: 'Campus board'});
-        addTest('deadHangEdge', {type: 'trial', english: 'Dead hang edge', unit: 'mm'});
-        addTest('deadHangPocket', {type: 'trial', english: 'Dead hang pocket'});
-        addTest('deadHangSloper', {type: 'trial', english: 'Dead hang sloper'});
+        addTest('crimp', {type: 'trial', english: 'crimp', unit: 'mm'});
+        addTest('openHand', {type: 'trial', english: 'openHand'});
+        addTest('sloper35Degrees', {type: 'trial', english: 'sloper35Degrees'});
+        addTest('sloper45Degrees', {type: 'challenge', english: 'sloper45Degrees'});
+        addTest('onefingerPocket', {type: 'challenge', english: 'onefingerPocket'});
+        addTest('twofingerPocket', {type: 'trial', english: 'twofingerPocket'});
+        addTest('dipsBars', {type: 'trial', english: 'dipsBars'});
         addTest('dipsRings', {type: 'trial', english: 'Dips rings'});
-        addTest('dragonFlags', {type: 'trial', english: 'Dragon flags'});
+        addTest('dragonFlags', {type: 'challenge', english: 'Dragon flags'});
+        addTest('humanFlag', {type: 'challenge', english: 'humanFlag'});
         addTest('height', {type: 'measurement', english: 'Height', unit: 'cm'});
         addTest('ironCross', {type: 'challenge', english: 'Iron cross'});
         addTest('legLength', {type: 'measurement', english: 'Leg length', unit: 'cm'});
         addTest('legLifts', {type: 'trial', english: 'Leg lifts'});
-        addTest('onearms', {type: 'trial', english: 'Onearms'});
+        addTest('onearms', {type: 'challenge', english: 'Onearms'});
         addTest('personalBestBoulderingFlash', {type: 'measurement', english: 'Personal best bouldering flash', isGrade: true, gradeType: 'bouldering'});
         addTest('personalBestBoulderingRedpoint', {type: 'measurement', english: 'Personal best bouldering redpoint', isGrade: true, gradeType: 'bouldering'});
         addTest('personalBestLeadFlash', {type: 'measurement', english: 'Personal best lead flash', isGrade: true, gradeType: 'lead'});
         addTest('personalBestLeadRedpoint', {type: 'measurement', english: 'Personal best lead redpoint', isGrade: true, gradeType: 'lead'});
         addTest('pullups', {type: 'trial', english: 'Pull-ups'});
         addTest('pushups', {type: 'trial', english: 'Push-ups'});
-        addTest('stickSitups', {type: 'trial', english: 'Stick sit-ups'});
         addTest('stableBoulderingFlash', {type: 'measurement', english: 'Stable bouldering flash', isGrade: true, gradeType: 'bouldering'});
         addTest('stableBoulderingRedpoint', {type: 'measurement', english: 'Stable bouldering redpoint', isGrade: true, gradeType: 'bouldering'});
         addTest('stableLeadFlash', {type: 'measurement', english: 'Stable lead flash', isGrade: true, gradeType: 'lead'});
         addTest('stableLeadRedpoint', {type: 'measurement', english: 'Stable lead redpoint', isGrade: true, gradeType: 'lead'});
         addTest('typewriters', {type: 'trial', english: 'Typewriters'});
         addTest('weight', {type: 'measurement', english: 'Weight', unit: 'kg'});
+        addTest('frontLever', {type: 'challenge', english: 'frontLever', unit: 'kg'});
+        addTest('muscleupsRings', {type: 'challenge', english: 'muscleupsRings', unit: 'kg'});
+        addTest('muscleupsBar', {type: 'challenge', english: 'muscleupsBar', unit: 'kg'});
     });
 
     tests
